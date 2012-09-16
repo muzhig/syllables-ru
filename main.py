5267b42b67dfc3b5420e96469650f573e4f8a24a
muzhig <mail@muzhig.ru>
2012-09-16 17:51:47 +0600

    больше красоты

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #coding=utf-8
 import re
 vowels = set(u'аеёиоуыэюя')
-pattern_str = u"(c*[ьъ]?vc+[ьъ](?=v))|(c*[ьъ]?v(?=v|cv))|(c*[ьъ]?vc[ъь]?(?=cv|ccv))|(c*[ьъ]?v[cьъ]*(?=$))"
-
 sign_chars = set(u'ъь')
+pattern = re.compile(u"(c*[ьъ]?vc+[ьъ](?=v))|(c*[ьъ]?v(?=v|cv))|(c*[ьъ]?vc[ъь]?(?=cv|ccv))|(c*[ьъ]?v[cьъ]*(?=$))")
+
+def get_syllables(word):
+    mask = ''.join(['v' if c in vowels else c if c in sign_chars else 'c' for c in word.lower()])
+    return [word[m.start():m.end()] for m in pattern.finditer(mask)]
 
 word = raw_input('слово:').decode('utf-8')
-
-mask = ''.join(['v' if c in vowels else c if c in sign_chars else 'c' for c in word.lower()])
-print mask
-
-syllables = [word[m.start():m.end()] for m in re.finditer(pattern_str,mask)]
-print '-'.join(syllables)
+print '-'.join(get_syllables(word))
 
